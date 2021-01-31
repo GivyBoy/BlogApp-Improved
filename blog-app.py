f0a7c0d866a66baf09fbb224a8c5177fc6bd76bf
Anthony Givans <58392771+GivyBoy@users.noreply.github.com>
2021-01-31 12:34:03 -0500

    Update blog-app.py

--- blog-app.py
+++ blog-app.py
@@ -9,7 +9,7 @@ plt.style.use('seaborn-whitegrid')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.write("""# Portfolio Optimization""")
-st.write("This web app seeks to construct the best weighted portfolio, given the names of a portfolio of stocks")
+st.write("This web application seeks to construct the best weighted portfolio, using the Modern Portfolio Theory and the Efficient Markets Hypothesis, given the stock tickers of a portfolio of stocks")
 
 if st.checkbox('Tap/Click to see examples of stock tickers'):
   examples = pd.read_csv('stocks.csv')
@@ -76,7 +76,7 @@ if len(stock_data.columns) > 0:
   
   st.write("""## Efficient Markets Frontier""")
   st.write(f'The number of portfolios being used is: {number_of_portfolios}')
-  st.write("Data from 2015 - 2019 is used in the analysis below.")
+  st.write("Data from 2015 - 2019 is being used in the analysis below.")
 
   for portfolio in range(number_of_portfolios):
           # generate a w random weight of length of number of stocks
@@ -194,6 +194,7 @@ if len(stock_data.columns) > 0:
   
   st.write("""## BACKTESTING""")
   st.write("The Weighted Portfolio is what the model above recommends, while the Equal Weighted Portfolio assumes that the portfolio is split evenly among all stocks.")
+  st.write("The data being used is from the start of the trading year in 2020, until the most recent completed trading day. It serves as a means of testing whether the recommendations given by the model is profitable, relative to some other metrics.")
            
   plt.figure(figsize=(10, 5))
   plt.plot(AdjustedR['Portfolio Value - Weighted'], c='red', label='Weighted Portfolio Value')
